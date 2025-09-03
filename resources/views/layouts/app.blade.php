fa96c612e94e6d5bb32785f4ea4deb1ed5024d04
Lgzinmarec <135389290+Lgzinmarec@users.noreply.github.com>
2025-09-03 15:04:29 -0300

    PAGINACAO2.0

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,41 +10,82 @@
     
     
     <style>
-        body {
-            background-color: #1b5c83; 
-            color: #fff;
-        }
-        .navbar {
-            background-color: #1b5c83; 
-        }
-        .btn-primary {
-            background-color: #E87B14; 
-            border: none;
-            color: #004b87;
-        }
-        .btn-primary:hover {
-            background-color: #E87B14;
-            color: #1b5c83;
-        }
-        .container {
-            margin-top: 2rem;
-            background: #ffffff;
-            padding: 2rem;
-            border-radius: 8px;
-            color: #1b5c83;
-        }
-        footer {
-            margin-top: 2rem;
-            padding: 1rem 0;
-            background-color: #1b5c83;
-            color: #ffffff;
-            text-align: center;
-        }
-       
-        .logo-ceb {
-            height: 40px;
-        }
-    </style>
+    body {
+        background-color: #1b5c83; 
+        color: #fff;
+    }
+
+    .navbar {
+        background-color: #1b5c83; 
+    }
+
+    .btn-primary {
+        background-color: #E87B14; 
+        border: none;
+        color: #004b87;
+    }
+
+    .btn-primary:hover {
+        background-color: #E87B14;
+        color: #1b5c83;
+    }
+
+    .container {
+        margin-top: 2rem;
+        background: #ffffff;
+        padding: 2rem;
+        border-radius: 8px;
+        color: #1b5c83;
+    }
+
+    footer {
+        margin-top: 2rem;
+        padding: 1rem 0;
+        background-color: #1b5c83;
+        color: #ffffff;
+        text-align: center;
+    }
+
+    .logo-ceb {
+        height: 40px;
+    }
+
+  
+    .pagination {
+        display: flex;
+        justify-content: center;
+        padding-left: 0;
+        list-style: none;
+        border-radius: 0.375rem;
+    }
+
+    .pagination .page-item .page-link {
+        color: #E87B14;
+        border: 1px solid #E87B14;
+        padding: 0.5rem 0.75rem;
+        margin: 0 2px;
+        background-color: #fff;
+        transition: all 0.3s ease-in-out;
+    }
+
+    .pagination .page-item.active .page-link {
+        background-color: #E87B14;
+        border-color: #E87B14;
+        color: #fff;
+    }
+
+    .pagination .page-item .page-link:hover {
+        background-color: #e87b14cc;
+        color: #fff;
+    }
+
+    .pagination .page-item.disabled .page-link {
+        color: #ccc;
+        pointer-events: none;
+        background-color: #f8f9fa;
+        border-color: #dee2e6;
+    }
+</style>
 </head>
 <body>
     <nav class="navbar navbar-expand-lg">
